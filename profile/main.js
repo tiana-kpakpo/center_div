d4ab210be6a1ace8da3b6b8363e3a00c1f0fddea
Christiana Adorkor Kpakpo <cWX1169406@china.huawei.com>
2023-07-13 16:20:59 +0000

    ui update

---
+++ profile/main.js
@@ -0,0 +1,18 @@
+// alert("javascript is working")
+
+const uploadButton = document.querySelector('div.img-profile div.iconDiv');
+const uploadInput = document.querySelector('.trick-upload input[type=file]');
+
+uploadButton.addEventListener('click', function() {
+    uploadInput.click();
+});
+
+
+const readText = () => {
+
+    alert("hello I am ready to ready text")
+
+}
+
+
+// readText();
