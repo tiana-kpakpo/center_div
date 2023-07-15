ef7af8ba638ded95ab8efa54deaae2d61a60cb43
Christiana Adorkor Kpakpo <cWX1169406@china.huawei.com>
2023-07-15 11:11:50 +0000

    video

--- profile/main.js
+++ profile/main.js
@@ -1,18 +1,54 @@
-// alert("javascript is working")
 
-const uploadButton = document.querySelector('div.img-profile div.iconDiv');
-const uploadInput = document.querySelector('.trick-upload input[type=file]');
-
-uploadButton.addEventListener('click', function() {
-    uploadInput.click();
-});
+// ------------------- wait for all element to load ------------------- 
+$(document).ready(function() {
 
 
-const readText = () => {
-
-    alert("hello I am ready to ready text")
-
-}
-
-
-// readText();
+    console.log("main javascript file is working")
+    
+    const uploadButton = document.querySelector('div.img-profile div.iconDiv');
+    
+    uploadButton.addEventListener('click', function() {
+        document.querySelector('input[type=file]').click();
+    });
+    
+    
+    $('.submit-update').click(function (e) { 
+        e.preventDefault();
+        
+        // ---------------------span  selecting elements from our webpage----------------------------
+       let student_name =  $('span#name');
+       let school = $('span#school');
+       let profile_email = $('span#email');
+       let profile_phone = $('span#contact');
+    
+    
+    // ---------------------- get form data --------------------------
+       let first_name = $('input#firstname').val();
+       let last_name = $('input#lastname').val();
+        let school_name = $('input#school').val();
+        let email = $('input#email').val();
+        let phone = $('input#contact').val();
+    
+        student_name.text('');
+        student_name.text(first_name+ " " +last_name);
+        school.text('');
+        school.text(school_name);
+        profile_email.text('');
+        profile_email.text(email);
+        profile_phone.text('');
+        profile_phone.text(phone);
+    
+    
+    
+    
+        
+    });
+    
+    
+    
+    
+    });
+    
+    // ------------------- wait for all element to load ends ------------------- 
+    
+    
