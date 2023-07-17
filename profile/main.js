df6359e5f9c8ea3b2083869000ef2f24783dca97
Christiana Adorkor Kpakpo <cWX1169406@china.huawei.com>
2023-07-17 00:26:33 +0000

    refactor

--- profile/main.js
+++ profile/main.js
@@ -1,37 +1,43 @@
-$(document).ready(function() {
-    console.log("main javascript file is working")
-  
-    const uploadButton = document.querySelector('div.img-profile div.iconDiv');
-  
-    uploadButton.addEventListener('click', function() {
-      $('input[type=file]').click();
+$(document).ready(function () {
+    console.log("main JavaScript file is working");
+
+    const uploadButton = document.querySelector('.img-profile .iconDiv');
+
+    uploadButton.addEventListener('click', function () {
+        $('input[type=file]').click();
     });
-  
-    $('input[type=file]').change(function() {
-      let filePath = $(this).val();
-      let fileName = filePath.split('\\').pop().split('/').pop();
-      console.log(filePath);
-      let img = $('div.img-profile.img.image');
-      img.attr('src', filePath);
+
+    $('input[type=file]').change(function (e) {
+        let file = e.target.files[0];
+        let reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onload = function (e) {
+            let img = $('.img-profile .image');
+            img.attr('src', e.target.result);
+        };
+
+
     });
-  
-    $('.submit-update').click(function(e) {
-      e.preventDefault();
-  
-      let student_name = $('span#name');
-      let school = $('span#school');
-      let profile_email = $('span#email');
-      let profile_phone = $('span#contact');
-  
-      let first_name = $('input#firstname').val();
-      let last_name = $('input#lastname').val();
-      let school_name = $('input#school').val();
-      let email = $('input#email').val();
-      let phone = $('input#contact').val();
-  
-      student_name.text(first_name + " " + last_name);
-      school.text(school_name);
-      profile_email.text(email);
-      profile_phone.text(phone);
+
+    $('.submit-update').click(function (e) {
+        e.preventDefault();
+
+        let student_name = $('#name');
+        let school = $('#school');
+        let profile_email = $('#email');
+        let profile_phone = $('#contact');
+
+        let first_name = $('#firstname').val();
+        let last_name = $('#lastname').val();
+        let school_name = $('#school').val();
+        let email = $('#email').val();
+        let phone = $('#contact').val();
+
+        student_name.text(first_name + " " + last_name);
+        school.text(school_name);
+        profile_email.text(email);
+        profile_phone.text(phone);
     });
-  });
+});
