d9732c4c2b8843619d84c0a799af0943d3c746da
tiana-kpakpo <tianakpakpo1@gmail.com>
2023-08-03 13:35:40 +0000

    test

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -9,56 +9,22 @@ const info = {
     occupation: 'Software Engineer',
     hobbies: ['Reading', 'Gardening', 'Playing Guitar'],
     address: {
+      country: 'USA',
       city: 'New York',
-      country: 'USA'
+      county:'Brooklyn',
     },
     favoriteColors: ['Green', 'Purple', 'Blue']
   };
 
-  console.log(info)
+//   console.log(info)
 
 
 // turn that object created above into an array
+// BODMAS
 
-let arryinfo = Object.entries(info)
-console.log(arryinfo)
-
-let valuearry = Object.values(info)
-console.log(valuearry)
+let arryInfo = Object.entries(info).sort()
+console.log(arryInfo)
 
 
-// sort the array and console.log(results)
-const sortedinfo = arryinfo.sort((a, b) => {
-    return a - b;
-  })
-  console.log(sortedinfo)
-
-  const sortedvalues = valuearry.sort((a, b) => {
-    return a - b;
-  })
-  console.log(sortedvalues)
 
 
-  // Convert inner objects into arrays
-for (const key in info) {
-    if (Object.hasOwnProperty.call(info, key)) {
-      info[key] = Object.values(info[key]);
-    }
-  }
-  
-  console.log(info);
-
-//   // Convert inner objects into arrays, leave other properties as they are
-// for (const [key, value] of Object.entries(info)) {
-//     if (typeof value === 'object' && !Array.isArray(value)) {
-//       info[key] = Object.values(value);
-//     }
-//   }
-  
-//   console.log(info);
-
-
-// Filter and convert the 'address' property's inner object into an array
-const addressArray = Object.values(info.address);
-
-console.log(addressArray);
